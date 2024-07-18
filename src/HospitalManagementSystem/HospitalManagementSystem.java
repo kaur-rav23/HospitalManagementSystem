1dd73ebfd781add1d64ad7f795a676b551bb26b0
kaur-rav23 <102215556+kaur-rav23@users.noreply.github.com>
2024-07-18 23:55:01 +0530

    Update HospitalManagementSystem.java
    Add your database password

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HospitalManagementSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
-    private static final String password = "Kaur@rav523";
+    private static final String password = "{addd your own password}";
 
     public static void main(String[] args) {
         try{
